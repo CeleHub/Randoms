cf7b6a32c3987bae76140988d5e747fa77ced288
Celestine <celestine4321@gmail.com>
2024-12-16 14:14:47 +0100

    Initial commit

---
+++ Randoms.java
@@ -0,0 +1,107 @@
+// import java.util.Scanner;
+// code to calculate factorial
+// public class Factorial{
+//     public ()
+
+
+//     public static void main (string [] Args){
+
+//     }
+// }
+
+// import java.util.Scanner;
+// public class Randoms{
+
+//     public static void main(String[] Args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.print("First Number: ");
+//         Double a = scanner. nextDouble();
+
+//         System.out.print("Second Number: ");
+//         Double b = scanner. nextDouble();
+
+//         Double sum = a + b;
+
+//         System.out.println("The sum is " + sum);
+
+//     }
+// }
+
+// public class Randoms {
+
+//     public static void main(String[] args) {
+//         int a = 40;
+//         int b = 20;
+
+//         // Checking for '=='
+//         if (a==b){
+//             System.out.println("Well, " + a + " is equal to " + b);
+//         }
+//         // else {
+//         //     System.out.println("Well, " + a + " is not equal to " + b);
+//         // }
+
+//         // Checking for '!='
+//         if (a!=b){
+//             System.out.println("Truly, " + a + " is not equal to " + b);
+//         }
+//         // else {
+//         //     System.out.println("Truly, " + a + " is equal to " + b);
+//         // }
+
+//         //Checking for '>'
+//         if (a>b){
+//             System.out.println(a + " is greater than " + b);
+//         }
+
+//         // Checking for '<'
+//         if (a<b){
+//             System.out.println(a + " is less than " + b);
+//         } 
+
+//         // Checking for '>='
+//         if (a>=b){
+//             System.out.println(a + " is greater than or equal to than " + b);
+//         } 
+
+//         // Checking for '<='
+//         if (a<=b){
+//             System.out.println(a + " is less than or equal to " + b);
+//         } 
+//     }
+// }
+
+// public class Randoms {
+
+//     public static void main(String[] args) {
+//         boolean a = true;
+//         boolean b = false;
+
+//         // Demonstrating '&&'
+//         System.out.println("a AND b = " + (a && b));
+
+//         // Demonstrating '||'
+//         System.out.println("a OR b = " + (a || b));
+
+//         //Demonstrating '!'
+//         System.out.println("NOT a = " + (!a));
+//         System.out.println("NOT b = " + (!b));
+
+
+//     }
+// }
+
+// // Package Example - JOptionPane;
+// import javax.swing.JOptionPane;
+// public class Randoms {
+//     public static void main(String[] args) {
+//     String surname = JOptionPane.showInputDialog("Surname: ", "Enter your Surname");
+//     String firstname = JOptionPane.showInputDialog("First Name: ", "Enter your First Name");
+//     String middlename = JOptionPane.showInputDialog("Middle Name: ", "Enter your Middle Name");
+
+//     String fullname = "OUR UNVERSITY IS " + surname + " " + firstname + " " + middlename;
+//     JOptionPane.showMessageDialog(null, fullname, "Name", JOptionPane.WARNING_MESSAGE);
+//     System.exit(0);
+//     }
+// }
